a346c9fcf9e01c3b73bf87d5df15519a4b098263
Felipe Boff <felipe.boff3@gmail.com>
2025-06-12 19:39:34 -0300

    chore(eslint): configure prettier and add dependencies for code formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,32 @@
+// eslint.config.js
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,cjs,mjs,ts,cts,mts}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      // se quiser usar regras específicas no futuro
+    },
+    rules: {
+      // Regras personalizadas aqui
+    },
+  },
+  js.configs.recommended,
   tseslint.configs.recommended,
+  {
+    rules: {
+      ...prettier.rules, // Desativa regras conflitantes com Prettier
+    },
+  },
 ]);
