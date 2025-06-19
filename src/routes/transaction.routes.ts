41b1ff629e094771c444cb7026d437237e9afca9
Felipe Boff <felipe.boff3@gmail.com>
2025-06-19 15:11:42 -0300

    feat: add route to retrieve all transactions

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 
 import { makeCreateTransactionController } from "@/factories/transaction/create-transaction.factory";
+import { makeGetAllTransactionsController } from "@/factories/transaction/get-all-transactions.factory";
 import { makeUpdateTransactionController } from "@/factories/transaction/update-transaction.factory";
+
 const router = Router();
+
+router.get("/", async (req, res) => {
+  await makeGetAllTransactionsController().execute(res);
+});
 
 router.post("/", async (req, res) => {
   await makeCreateTransactionController().execute(req, res);
