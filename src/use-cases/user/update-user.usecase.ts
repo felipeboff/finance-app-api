76dc2ff9538c13c2afb7669ed1f09f7c72a33023
Felipe Boff <felipe.boff3@gmail.com>
2025-06-19 11:56:56 -0300

    refactor: streamline user update logic by adjusting

--- src/use-cases/user/update-user.usecase.ts
+++ src/use-cases/user/update-user.usecase.ts
@@ -1,7 +1,6 @@
 import { UserPostgresRepository } from "@/repositories/user-postgres.repository";
 import { UserNotFoundError } from "@/errors/user.error";
-import { UserEntity } from "@/entities/user.entity";
-import { EmailAlreadyExistsError } from "@/errors/shared.error";
+import { EmailAlreadyExistsError } from "@/errors/user.error";
 import { hashPassword } from "@/services/hash.service";
 import { UpdateUserDTO } from "@/dtos/user.dto";
 
@@ -14,19 +13,16 @@ export class UpdateUserUseCase {
     if (input.email) {
       const userEmailExists = await this.userRepo.findByEmail(input.email);
       if (userEmailExists && userEmailExists.id !== user.id) {
-        throw new EmailAlreadyExistsError(input.email);
+        throw new EmailAlreadyExistsError();
       }
     }
 
-    const safeUpdateValue: UserEntity = {
+    const safeUpdateValue: UpdateUserDTO = {
+      ...input,
       id: user.id,
-      first_name: input.first_name ?? user.first_name,
-      last_name: input.last_name ?? user.last_name,
-      email: input.email ?? user.email,
       password: input.password
         ? await hashPassword(input.password)
         : user.password,
-      created_at: user.created_at,
     };
 
     return await this.userRepo.update(safeUpdateValue);
