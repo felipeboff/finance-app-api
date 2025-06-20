93484c062d8f6dd4da842af12ba1765a5983ecbf
Felipe Boff <felipe.boff3@gmail.com>
2025-06-20 00:27:28 -0300

    refactor: simplify input validation in UpdateTransactionController using Zod schema

--- src/controllers/transaction/update-transaction.controller.ts
+++ src/controllers/transaction/update-transaction.controller.ts
@@ -7,47 +7,26 @@ import {
   ok,
   serverError,
 } from "@/helpers/http-response.helper";
+import { updateTransactionSchema } from "@/schema/transaction.schema";
 import { UpdateTransactionUseCase } from "@/use-cases/transaction/update-transaction.usecase";
-import {
-  hasUnexpectedFields,
-  isValidBody,
-  isValidUUID,
-} from "@/validators/shared.validator";
-import { isValidAmount } from "@/validators/transactions.validator";
+import { isValidUUID } from "@/validators/shared.validator";
 
 export class UpdateTransactionController {
   constructor(private readonly useCase: UpdateTransactionUseCase) {}
 
   async execute(req: Request, res: Response): Promise<Response> {
     try {
-      if (!isValidBody(req.body)) {
-        return badRequest(res, "Request body is empty");
-      }
-
       const { transactionId } = req.params;
-      const { title, date, amount, type, ...rest } = req.body;
 
       if (!isValidUUID(transactionId)) {
         return badRequest(res, "Invalid transaction ID");
       }
 
-      if (hasUnexpectedFields(rest)) {
-        return badRequest(
-          res,
-          `Invalid request fields: ${Object.keys(rest).join(", ")}`,
-        );
-      }
-
-      if (amount && !isValidAmount(amount)) {
-        return badRequest(res, "Invalid amount");
-      }
+      const input = await updateTransactionSchema.parseAsync(req.body);
 
       const updatedTransaction = await this.useCase.execute({
         id: transactionId,
-        title,
-        date,
-        amount,
-        type,
+        ...input,
       });
 
       if (!updatedTransaction) {
