f633d7a6a170ee98a3a75ad3eef2e82ac8319608
Felipe Boff <felipe.boff3@gmail.com>
2025-06-19 23:55:10 -0300

    refactor: enhance UserSchema with strict validation and add update as a partial version

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,16 +1,20 @@
 import { z } from "zod";
 
-export const createUserSchema = z.object({
-  first_name: z
-    .string({ required_error: "First name is required" })
-    .nonempty("First name is required"),
-  last_name: z
-    .string({ required_error: "Last name is required" })
-    .nonempty("Last name is required"),
-  email: z
-    .string({ required_error: "Email is required" })
-    .email("Invalid email address"),
-  password: z
-    .string({ required_error: "Password is required" })
-    .min(6, "Password must be at least 6 characters long"),
-});
+export const createUserSchema = z
+  .object({
+    first_name: z
+      .string({ required_error: "First name is required" })
+      .nonempty("First name is required"),
+    last_name: z
+      .string({ required_error: "Last name is required" })
+      .nonempty("Last name is required"),
+    email: z
+      .string({ required_error: "Email is required" })
+      .email("Invalid email address"),
+    password: z
+      .string({ required_error: "Password is required" })
+      .min(6, "Password must be at least 6 characters long"),
+  })
+  .strict();
+
+export const updateUserSchema = createUserSchema.partial();
