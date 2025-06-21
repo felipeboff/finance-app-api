d7dac308fbfea0ea3a0fcdfc05fcdc2442c57808
Felipe Boff <felipe.boff3@gmail.com>
2025-06-21 18:32:12 -0300

    test: add validation tests for CreateUserController to handle missing email and password

--- src/controllers/transaction/create-user.test.ts
+++ src/controllers/transaction/create-user.test.ts
@@ -77,4 +77,74 @@ describe("CreateUserController", () => {
 
     expect(status).toHaveBeenCalledWith(400);
   });
+
+  it("should respond with 400 if email is not provided", async () => {
+    const req = {
+      body: {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        password: faker.internet.password({ length: 10 }),
+      },
+    } as Request;
+
+    const { res, status } = mockResponse();
+
+    const controller = new CreateUserController(new CreateUserUseCaseStub());
+    await controller.execute(req, res);
+
+    expect(status).toHaveBeenCalledWith(400);
+  });
+
+  it("should respond with 400 if password is not provided", async () => {
+    const req = {
+      body: {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+      },
+    } as Request;
+
+    const { res, status } = mockResponse();
+
+    const controller = new CreateUserController(new CreateUserUseCaseStub());
+    await controller.execute(req, res);
+
+    expect(status).toHaveBeenCalledWith(400);
+  });
+
+  it("should respond with 400 if password length is less than 6 characters", async () => {
+    const req = {
+      body: {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 5 }),
+      },
+    } as Request;
+
+    const { res, status } = mockResponse();
+
+    const controller = new CreateUserController(new CreateUserUseCaseStub());
+    await controller.execute(req, res);
+
+    expect(status).toHaveBeenCalledWith(400);
+  });
+
+  it("invalid email format", async () => {
+    const req = {
+      body: {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: "invalid-email",
+        password: faker.internet.password({ length: 10 }),
+      },
+    } as Request;
+
+    const { res, status } = mockResponse();
+
+    const controller = new CreateUserController(new CreateUserUseCaseStub());
+    await controller.execute(req, res);
+
+    expect(status).toHaveBeenCalledWith(400);
+  });
 });
