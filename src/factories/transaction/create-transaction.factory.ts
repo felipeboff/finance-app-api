3978bd053a6f2d8bec10331abb627595767cfb32
Felipe Boff <felipe.boff3@gmail.com>
2025-07-01 18:49:50 -0300

    refactor: update CreateTransactionController to include UserPostgresRepository in use case

--- src/factories/transaction/create-transaction.factory.ts
+++ src/factories/transaction/create-transaction.factory.ts
@@ -1,10 +1,12 @@
 import { CreateTransactionController } from "@/controllers/transaction/create-transaction.controller";
 import { TransactionsPostgresRepository } from "@/repositories/transaction.repository";
+import { UserPostgresRepository } from "@/repositories/user.repository";
 import { CreateTransactionUseCase } from "@/use-cases/transaction/create-transaction.usecase";
 
 export const makeCreateTransactionController =
   (): CreateTransactionController => {
     const repo = new TransactionsPostgresRepository();
-    const useCase = new CreateTransactionUseCase(repo);
+    const userRepo = new UserPostgresRepository();
+    const useCase = new CreateTransactionUseCase(repo, userRepo);
     return new CreateTransactionController(useCase);
   };
