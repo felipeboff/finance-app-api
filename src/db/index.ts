3d4d86655dd98906c35f17119e118304a566e075
Felipe Boff <felipe.boff3@gmail.com>
2025-07-07 21:31:52 -0300

    feat: add database connection test to ensure successful connection to PostgreSQL

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,16 @@
 import "dotenv/config";
 
+import { sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 export const db = drizzle(process.env.DATABASE_URL!);
+
+async function testConnection() {
+  try {
+    await db.execute(sql`SELECT 1`);
+    console.log("Conexão com o banco de dados bem-sucedida.");
+  } catch {
+    console.error("Falha na conexão com o banco de dados:");
+  }
+}
+testConnection();
