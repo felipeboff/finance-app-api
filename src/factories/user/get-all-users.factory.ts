0e5ffaaa012f13a42b9e1a69b076ed3b49c35966
Felipe Boff <felipe.boff3@gmail.com>
2025-07-07 22:17:44 -0300

    refactor: change filename get all users controller

--- src/factories/user/get-all-users.factory.ts
+++ src/factories/user/get-all-users.factory.ts
@@ -1,4 +1,4 @@
-import { GetAllUserController } from "@/controllers/user/get-all-user.controller";
+import { GetAllUserController } from "@/controllers/user/get-all-users.controller";
 import { db } from "@/db";
 import { UserRepository } from "@/repositories/user.repository";
 import { GetAllUsersUseCase } from "@/use-cases/user/get-all-users.usecase";
