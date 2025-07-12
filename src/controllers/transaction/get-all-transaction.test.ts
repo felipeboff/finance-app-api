05634b7227fee9539478b54bcc6af6ae827ae505
Felipe Boff <felipe.boff3@gmail.com>
2025-07-12 13:07:19 -0300

    test: add unit tests for GetAllTransactionsController

---
+++ src/controllers/transaction/get-all-transaction.test.ts
@@ -0,0 +1,59 @@
+import { faker } from "@faker-js/faker/locale/pt_BR";
+import { Response } from "express";
+
+import { GetAllTransactionsController } from "@/controllers/transaction/get-all-transactions.controller";
+import { TransactionEntity } from "@/entities/transaction.entity";
+import { IGetAllTransactionsUseCase } from "@/use-cases/transaction/transaction.type";
+
+describe("GetAllTransactionsController", () => {
+  const transactions: TransactionEntity[] = Array.from({ length: 2 }, () => ({
+    id: faker.string.uuid(),
+    title: faker.lorem.sentence(),
+    date: faker.date.anytime(),
+    user_id: faker.string.uuid(),
+    amount: faker.number.int({ min: 1, max: 1000 }),
+    type: faker.helpers.arrayElement(["EARNING", "EXPENSE", "INVESTMENT"]),
+    created_at: faker.date.anytime(),
+  }));
+
+  class GetAllTransactionsUseCaseStub implements IGetAllTransactionsUseCase {
+    async execute(): Promise<TransactionEntity[]> {
+      return transactions;
+    }
+  }
+
+  const createSut = () => {
+    const useCase = new GetAllTransactionsUseCaseStub();
+    const controller = new GetAllTransactionsController(useCase);
+    return { useCase, controller };
+  };
+
+  const mockResponse = () => {
+    const json = jest.fn();
+    const status = jest.fn(() => ({ json }));
+    return { res: { status } as unknown as Response, status, json };
+  };
+
+  it("should return 200 with all transactions when use case returns transactions", async () => {
+    const { res, status, json } = mockResponse();
+    const { controller } = createSut();
+
+    await controller.execute(res);
+
+    expect(status).toHaveBeenCalledWith(200);
+    expect(json).toHaveBeenCalledWith(transactions);
+  });
+
+  it("should return 500 when use case throws error", async () => {
+    const { res, status } = mockResponse();
+    const { controller, useCase } = createSut();
+
+    jest.spyOn(useCase, "execute").mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    await controller.execute(res);
+
+    expect(status).toHaveBeenCalledWith(500);
+  });
+});
